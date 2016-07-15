74f0b3f7373f7adea1c0c7314cda8b48f57ed574
richardrun <richardrunlpl@gmail.com>
2016-07-15 09:49:13 +0800

    php test changed

--- text.php
+++ text.php
@@ -10,63 +10,32 @@
 
 echo "123213";
 
+$user = 'root';
+$password = 'root';
+$db = 'company';
+$host = 'localhost';
+$port = 8889;
+
+$link = mysqli_init();
+$success = mysqli_real_connect(
+    $link,
+    $host,
+    $user,
+    $password,
+    $db,
+    $port
+);
+echo $success?"success":"fail";
+
+$sql = "SELECT id FROM staff WHERE id=1 or 2 ";
+
+$result = $link->query($sql);
+
+$row = $result->fetch_row();
+printf("<br/>age=".$row[0]."<br/>");
+$link->close();
+
+echo "FINISH";
+
 //echo file_get_contents( "main_page.html" );
 ?>
-
-
-
-<div>
-    <img id="test1" src="src/test1.jpg" height="200" width="200" style="display:;">
-    <img id="test2" src="src/test2.jpg" height="200" width="200" style="display:none;">
-    <img id="test3" src="src/test3.jpg" height="200" width="200" style="display:none;">
-
-    <button onclick="previous();">Previous</button>
-    <button onclick="next()">Next</button>
-
-</div>
-
-
-<script type="text/javascript">
-
-    var oTest1 = document.getElementById("test1");
-    var oTest2 = document.getElementById("test2");
-    var oTest3 = document.getElementById("test3");
-
-    var container = [oTest1, oTest2, oTest3];
-
-    var index = 0;
-
-    function previous(){
-
-        if(index == 0){
-
-        } else if(index == 1){
-            container[0].style.display = '';
-            container[1].style.display = 'none';
-            container[2].style.display = 'none';
-            index--;
-        } else {
-            container[0].style.display = 'none';
-            container[1].style.display = '';
-            container[2].style.display = 'none';
-            index--;
-        }
-    }
-
-    function next(){
-
-        if(index==0){
-            container[0].style.display = 'none';
-            container[1].style.display = '';
-            container[2].style.display = 'none';
-            index++;
-        }else if(index==1){
-            container[0].style.display = 'none';
-            container[1].style.display = 'none';
-            container[2].style.display = '';
-            index++;
-        }else {
-        }
-    }
-
-</script>
