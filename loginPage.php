9b04ac62318426f3e933a962983700838702e41f
richardrun <richardrunlpl@gmail.com>
2016-07-15 17:53:39 +0800

    basic login interface

--- loginPage.php
+++ loginPage.php
@@ -1,3 +1,6 @@
+
+
+
 <?php
 /**
  * Created by PhpStorm.
@@ -5,3 +8,38 @@
  * Date: 7/15/16
  * Time: 2:20 PM
  */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login Page</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="css/loginPage.css">
+</head>
+<body>
+    <div class="main">
+        <h1>Press to Login in</h1>
+        <button><h1>User Login</h1></button>
+    </div>
+
+
+    <div class="popUp">
+        <div class="popUp-inner">
+            <form>
+                <fieldset>
+                    <legend>Login</legend>
+                    Name:     <input type="text" required> <br>
+                    Password: <input type="text" required> <br>
+                    <br>
+                    <input type="submit">
+                </fieldset>
+
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
