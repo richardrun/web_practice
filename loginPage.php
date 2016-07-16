c44ef8d9306a791cefa037a0ec25e3ed55daf990
richardrun <richardrunlpl@gmail.com>
2016-07-16 11:08:48 +0800

    updated login page with motion

--- loginPage.php
+++ loginPage.php
@@ -17,28 +17,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="js/loginPage.js"></script>
     <link rel="stylesheet" href="css/loginPage.css">
 </head>
 <body>
     <div class="main">
         <h1>Press to Login in</h1>
-        <button><h1>User Login</h1></button>
+        <button class="open"><h1>User Login</h1></button>
     </div>
 
 
     <div class="popUp">
-        <div class="popUp-inner">
-            <form>
-                <fieldset>
-                    <legend>Login</legend>
-                    Name:     <input type="text" required> <br>
-                    Password: <input type="text" required> <br>
-                    <br>
-                    <input type="submit">
-                </fieldset>
+    </div>
+    
+    <div class="popUp-inner">
+        <form>
+            <fieldset>
+                <legend>Login</legend>
+                <div>Name: <input type="text" required></div>
+                <div>Password: <input type="text" required></div>
+                <br>
+                <div> <input type="submit"></div>
+            </fieldset>
 
-            </form>
-        </div>
+        </form>
     </div>
 </body>
 
